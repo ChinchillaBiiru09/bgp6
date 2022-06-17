45dcff0bdf420cc08f0356342188641a9c21bcd4
ChinchillaBiiru09 <helfid0918@gmail.com>
2022-06-17 10:46:38 +0700

    bgp

---
+++ app/src/main/java/com/example/bgp_apps/ui/FaceLockActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bgp_apps.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bgp_apps.R
+
+class FaceLockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_face_lock)
+    }
+}
